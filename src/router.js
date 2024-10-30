8c134fe2b89f9e4b06158ab22c41ce11c4f0e83a
iTsGiuse <giuse.amato@icloud.com>
2024-10-30 18:35:30 +0100

    fix: vari fix

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Eventi from './pages/Eventi.vue';
 import Classifica from './pages/Classifica.vue';
 import ChiSiamo from './pages/ChiSiamo.vue';
 import NewsDettaglio from './pages/NewsDettaglio.vue';
+import EventoDettaglio from "./pages/EventoDettaglio.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -16,14 +17,19 @@ const router = createRouter({
             component: Home
         },
         {
-            path: '/news/:newsTitle',
+            path: '/news/:newsLink',
             name: 'news',
-            component: NewsDettaglio
+            component: NewsDettaglio,
         },
         {
             path: '/eventi', 
             name: 'eventi',
             component: Eventi
+        },
+        {
+            path: '/evento/:eventoLink',
+            name: 'evento',
+            component: EventoDettaglio,
         },
         {
             path: '/classifica', 
@@ -50,6 +56,16 @@ const router = createRouter({
         } else {
             return { left: 0, top: 0 };
         }
+    }
+});
+
+// Navigation guard globale per verificare i parametri extra
+router.beforeEach((to, from, next) => {
+    if (to.name === 'news' && Object.keys(to.params).length > 1) {
+        // Se ci sono parametri extra, reindirizza alla home (o un'altra pagina desiderata)
+        next({ path: '/' });
+    } else {
+        next();
     }
 });
 
