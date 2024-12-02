a2ef366c16ac385864af74d7725ae9a26381a73f
iTsGiuse <giuse.amato@icloud.com>
2024-12-02 23:59:11 +0100

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,9 +26,9 @@ export default{
                             <li class="nav-item">
                                 <router-link class="nav-link" active-class="active" to="/eventi">Eventi</router-link>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <router-link class="nav-link" active-class="active" to="/classifica">Classifica</router-link>
-                            </li>
+                            </li> -->
                             <li class="nav-item">
                                 <router-link class="nav-link" active-class="active" to="/chi-siamo">Chi Siamo</router-link>
                             </li>
